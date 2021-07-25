d8f2b5a10a41ba2ce940d7a39dd427cada53f34a
DaPotatoMan <iam.fahadul@gmail.com>
2021-07-25 11:09:06 +0600

    fix: build errors

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,6 +1,14 @@
 declare module '*.vue' {
    import type { DefineComponent } from 'vue';
 
-   const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
+   const component: DefineComponent<
+   Record<string, unknown>,
+   Record<string, unknown>,
+   unknown
+   >;
    export default component;
 }
+
+// Modules
+declare module '@windicss/plugin-scrollbar';
+declare module '@windicss/plugin-heropatterns';
