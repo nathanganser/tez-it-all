73778137c21c4350d236f6f9336cf6206d674439
DaPotatoMan <iam.fahadul@gmail.com>
2021-07-26 19:25:56 +0600

    feat: refactor stylesheets

--- src/components/pages/navbar/Navbar.vue
+++ src/components/pages/navbar/Navbar.vue
@@ -1,7 +1,11 @@
 <template>
    <header class="view-navbar">
       <div class="view-navbar-brand">
-         <img class="view-navbar-brand-logo" src="/assets/icons/logo.png" draggable="false">
+         <img
+            class="view-navbar-brand-logo"
+            src="/assets/icons/logo.png"
+            draggable="false"
+         >
          <span class="view-navbar-brand-label">TezItAll</span>
       </div>
 
@@ -11,27 +15,4 @@
    </header>
 </template>
 
-<style lang="postcss">
-.view-navbar {
-   @apply w-full px-6 py-2 z-5
-   flex items-center
-   bg-default-elevated border border-transparent border-b-black/10
-   sticky top-0 left-0;
-
-   &-brand {
-      @apply flex items-center;
-
-      &-logo {
-         @apply w-42px mr-4 md:w-48px;
-      }
-
-      &-label {
-         @apply text-xl sm:text-2xl font-medium;
-      }
-   }
-
-   &-links {
-      @apply ml-auto;
-   }
-}
-</style>
+<style lang="postcss" src="@assets/styles/pages/navbar/index.pcss"></style>
--- src/components/pages/navbar/NavbarFaq.vue
+++ src/components/pages/navbar/NavbarFaq.vue
@@ -13,63 +13,7 @@
    </div>
 </template>
 
-<style lang="postcss">
-.navbar-link-faq {
-   @apply relative z-11;
-
-   &-button {
-      @apply cursor-pointer;
-   }
-
-   .modal-wrapper {
-      @apply sm:relative;
-
-      &-overlay {
-         @apply fixed inset-0 -z-1
-         bg-black/20;
-      }
-
-      &-content {
-         @apply px-8 py-6 w-640px max-w-[90vw] max-h-[80vh]
-         content absolute top-2 sm:(right-0 max-h-[90vh])
-         overflow-hidden overflow-y-auto
-         bg-default-elevated rounded-xl shadow-md;
-
-         @screen <sm {
-            @apply top-[50%] left-[50%] px-6 py-4;
-            transform: translate(-50%, -50%);
-         }
-      }
-   }
-}
-
-.markdown-body {
-   h1,
-   h2 {
-      @apply mb-2 font-medium;
-   }
-
-   h1 {
-      @apply text-2xl;
-   }
-
-   h2 {
-      @apply text-[18px];
-   }
-
-   p {
-      @apply text-default/70 leading-relaxed;
-
-      &:not(:last-of-type) {
-         @apply mb-5;
-      }
-   }
-
-   a {
-      @apply font-medium underline text-accent;
-   }
-}
-</style>
+<style lang="postcss" src="@assets/styles/pages/navbar/navbar-faq.pcss"></style>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
