a7135b6e6b0a95e4d2d44383306ea8ec497f65f7
DaPotatoMan <iam.fahadul@gmail.com>
2021-07-29 09:13:24 +0600

    feat: Simplify dashboard faq

---
+++ src/components/pages/dashboard/ContractFaq.vue
@@ -0,0 +1,16 @@
+<template>
+   <FaqMD class="dashboard-contract-faq" />
+</template>
+
+<style lang="postcss" src="@assets/styles/pages/dashboard/contract-faq.pcss"></style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FaqMD from '/docs/faq.md';
+
+export default defineComponent({
+   components: {
+      FaqMD
+   }
+});
+</script>
