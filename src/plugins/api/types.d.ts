f8e79206f034436d62920ffb3df757a5fc748cf8
DaPotatoMan <iam.fahadul@gmail.com>
2021-08-18 14:00:15 +0600

    feat: Add notification popup

--- src/plugins/api/types.d.ts
+++ src/plugins/api/types.d.ts
@@ -9,3 +9,14 @@ interface WalletConnectionStatus {
    available: boolean;
    connected: boolean;
 }
+
+interface BidStatus {
+   state: boolean;
+   promise: Promise<{
+      block: BlockResponse;
+      expectedConfirmation: number;
+      currentConfirmation: number;
+      completed: boolean;
+      isInCurrentBranch: () => Promise<boolean>;
+   }>
+}
